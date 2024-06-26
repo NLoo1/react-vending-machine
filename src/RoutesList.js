67b7e5d7030445120a33280bcdb8c21e933dcfe8
Nicholas <NLoo1@outlook.com>
2024-06-26 17:45:08 -0400

    Added css, got routes to work

--- src/RoutesList.js
+++ src/RoutesList.js
@@ -1,11 +1,24 @@
 
-export function Candy() { }
+export function Candy() { 
+    return(
+        <div className="candy">
+            <h1>candy RAHHHH</h1>
+        </div>
+    )
+}
 
 export function Cookies() {
-
-
+    return(
+        <div className="cookies">
+            <h1>cookies RAHHHH</h1>
+        </div>
+    )
 }
 
 export function Chips() {
-
+    return(
+        <div className="chips">
+            <h1>CHIPS RAHHHH</h1>
+        </div>
+    )
 }
--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,22 +1,23 @@
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Link, Navigate } from "react-router-dom"
 import {Candy, Chips, Cookies} from './RoutesList'
+import './VendingMachine.css'
 
 function VendingMachine() {
   return (
     <div className="VendingMachine">
         <BrowserRouter>
         <nav>
+          <Link to='/'>Home</Link>
           <Link to='/chips'>Chips</Link>
           <Link to='/candy'>Candy</Link>
           <Link to='/cookies'>Cookies</Link>
-
         </nav>
-
-        <Switch>
+        <Routes>
           <Route path="/chips" element={<Chips />}/>
           <Route path="/candy" element={<Candy />}/>
           <Route path="/cookies" element={<Cookies />}/>
-        </Switch>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
         </BrowserRouter>
     </div>
   );
